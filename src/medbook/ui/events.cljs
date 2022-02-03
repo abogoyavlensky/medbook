52dcf2b803e749330b650c7afc5871bca8e4f6a0
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2022-02-03 23:36:54 +0300

    Add push-state effect

--- src/medbook/ui/events.cljs
+++ src/medbook/ui/events.cljs
@@ -3,6 +3,7 @@
             ;; import http-fx to register events
             [day8.re-frame.http-fx]
             [reitit.frontend.controllers :as reitit-controllers]
+            [reitit.frontend.easy :as reitit-easy]
             [medbook.ui.db :as db]))
 
 
@@ -21,6 +22,15 @@
                         new-match)]
       (assoc db :current-page (assoc new-match :controllers controllers)))))
 
+
+(re-frame/reg-fx :push-state
+  (fn [route]
+    (apply reitit-easy/push-state route)))
+
+
+(re-frame/reg-event-fx ::push-state
+  (fn [_ [_ & route]]
+    {:push-state route}))
 
 ;; Inspect app-db state
 (comment
