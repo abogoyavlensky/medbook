235c91636925c43001b6454a0b572df58e88cb15
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2022-02-06 00:13:36 +0300

    Split patient views by files

--- src/medbook/ui/patients/views/create.cljs
+++ src/medbook/ui/patients/views/create.cljs
@@ -1,1 +1,9 @@
 (ns medbook.ui.patients.views.create)
+
+
+(defn create-patient-view
+  "Render creating patient page."
+  [{:keys [current-route]}]
+  (let [page-title (get-in current-route [:data :page-title])]
+    [:div
+     [:h2 page-title]]))
--- src/medbook/ui/router.cljs
+++ src/medbook/ui/router.cljs
@@ -5,21 +5,22 @@
             [reitit.frontend.easy :as reitit-easy]
             [medbook.ui.events :as events]
             [medbook.ui.patients.events :as patients-events]
-            [medbook.ui.patients.views :as patients-views]))
+            [medbook.ui.patients.views.list :as patients-views-list]
+            [medbook.ui.patients.views.create :as patients-views-create]))
 
 
 (def ^:private routes
   ["/"
    [""
     {:name ::home
-     :view patients-views/patient-list-view
+     :view patients-views-list/patient-list-view
      :page-title "Patients"
      :controllers
      [{:start (fn [& _] (re-frame/dispatch [::patients-events/get-patients]))
        :stop  (fn [& _params] (js/console.log "Leaving home page"))}]}]
    ["create-patient"
     {:name ::create-patient
-     :view patients-views/create-patient-view
+     :view patients-views-create/create-patient-view
      :page-title "Create new patient"
      :controllers
      [{:start (fn [& _params] (js/console.log "Entering create page"))
