68886acb87c9b634f0e3bc05946e33fdd9eb97e5
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2022-02-11 08:51:38 +0300

    Remove comments from tests

--- test/medbook/ui_test.clj
+++ test/medbook/ui_test.clj
@@ -20,31 +20,17 @@
 
 
 (deftest test-front-page-not-found-ok
-  ;#p (slurp "http://localhost:8001")
   (testing "check when page not found"
-    ; TODO: move driver to ig component!
-    (let [;driver (etaoin/chrome-headless {:host "127.0.0.1"
-          ;                                :port 4444
-          ;                                :args ["--no-sandbox"]})
-          driver (get test-util/*test-system* ::test-util/chromedriver)]
-
+    (let [driver (get test-util/*test-system* ::test-util/chromedriver)]
       (etaoin/go driver (str test-util/TEST-URL-BASE "/wrong"))
-      ;(etaoin/go driver (str "http://test:8001" "/wrong"))
       (etaoin/wait-visible driver {:fn/has-text "MedBook"} {:timeout 5})
       (is (etaoin/visible? driver {:tag :h2
                                    :fn/text "Page not found."})))))
 
 
 (deftest test-wiki
-  (let [;driver (etaoin/chrome-headless {:host "127.0.0.1" :port 4444 :args ["--no-sandbox"]})]
-        driver (get test-util/*test-system* ::test-util/chromedriver)]
-    ;(etaoin/go driver "https://en.wikipedia.org/")
-    ;(etaoin/go driver "http://127.0.0.1:8001/")
-    ;(etaoin/go driver "http://test:8001/")
+  (let [driver (get test-util/*test-system* ::test-util/chromedriver)]
     (etaoin/go driver test-util/TEST-URL-BASE)
-    ;(etaoin/wait-visible driver [{:id :simpleSearch} {:tag :input :name :search}])
-    ;(etaoin/wait-visible driver {:fn/has-text "Patients"} {:timeout 5})
-    (etaoin/wait-visible driver {:tag :h2} {:timeout 3})
+    (etaoin/wait-visible driver {:fn/has-text "Patients"} {:timeout 5})
     (is (true? (etaoin/visible? driver {:tag :h2})))
-    ;#p (etaoin/get-title driver)))
     (etaoin/screenshot driver "page.png")))
